6e1188aaa51fa74ba0b1bbeec13637da0bc46dc5
MirnaxvdW <marnixvanderwerff@gmail.com>
2026-01-19 11:25:44 +0100

    Updates: Scores & Multipliers, Background update, UI changes, rework of layout

--- Assets/Scripts/ComboSystem.cs
+++ Assets/Scripts/ComboSystem.cs
@@ -1,35 +1,39 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 public class ComboSystem : MonoBehaviour
 {
     private List<string> bumperTags = new List<string>();   //lijst met geraakte tags
     private int scoreMultiplier = 1;
+
+      public static event Action<int, int> OnScoreChange;
     private void Start()
     {
         BumperHit.onBumperHit += CheckForCombo;             //luisteren naar action event onBumperHit als game start
     }
-    private void OnDisable()
+
+        private void OnDisable()
     {
         BumperHit.onBumperHit -= CheckForCombo;             //stop met luisteren naar action event onBumperHit als scene herstart of game stopt
     }
-    private void CheckForCombo(string tag, int bumperValue)
-    {
-        bumperTags.Add(tag);                                //tag toevoegen aan lijst
-        if (bumperTags.Count > 1)                           //check of er meer dan 1 tag is
-        {                                                   //check of de laatste 2 tags gelijk zijn
+
+     private void CheckForCombo(string tag, int bumperValue) {
+        
+        bumperTags.Add(tag);
+        if (bumperTags.Count > 1) {
             if (bumperTags[bumperTags.Count - 2] == bumperTags[bumperTags.Count - 1])
             {
-                scoreMultiplier++;                          //verhoog de multiplier
+                scoreMultiplier++;
             }
-            else                                            //als ze niet gelijk zijn
+            else
             {
-                scoreMultiplier = 1;                        //reset multiplier
-                bumperTags.Clear();                         //leeg de lijst met tags
+                scoreMultiplier = 0;
             }
-        }                                                   //voeg score toe aan de ScoreManager
+        }
         ScoreManager.Instance.AddScore(bumperValue * scoreMultiplier);
-
-        //print score en multiplier in de console
-        Debug.Log($"Score: {ScoreManager.Instance.score} || Multiplier: {scoreMultiplier}X");
+        //in plaats van de score en de multiplier naar de console te sturen met debug.Log gaan we vanaf hier een Action Event Versturen en geven we de score em de multiplier mee als argument.
+        OnScoreChange?.Invoke(ScoreManager.Instance.score, scoreMultiplier);
+        //Verwijder de Debug.Log regel!
+        //Debug.Log($"Score: {ScoreManager.Instance.score} || Multiplier: {scoreMultiplier}X");
     }
 }
